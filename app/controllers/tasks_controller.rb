25d3a6dbe95b996212a9880cc5eca91f6312f3bd
wataru.nakamura <soratotenchan@gmail.com>
2019-10-03 19:04:06 +0000

    kadai-tasklist-login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,11 @@
 class TasksController < ApplicationController
+ before_action :require_user_logged_in, only: [:index,:new,:edit]    
+ before_action :correct_user, only: [:destroy, :update, :show]
+    
     def index
-        @tasks = Task.all
+        if logged_in?
+          @tasks = current_user.tasks.order(id: :desc)
+        end
     end
     
     def show
@@ -12,7 +17,7 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
           flash[:success] = 'task が正常に投稿されました'
@@ -53,6 +58,13 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content, :status)
-    end    
+    end  
+    
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
     
 end
