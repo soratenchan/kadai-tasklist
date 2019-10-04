458dccf8f6418eb459062df98c3ec6ec5dae8228
wataru.nakamura <soratotenchan@gmail.com>
2019-10-04 06:45:24 +0000

    show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
- before_action :require_user_logged_in, only: [:index,:new,:edit]    
+ before_action :require_user_logged_in, only: [:index,:new,:edit,:show]    
  before_action :correct_user, only: [:destroy, :update, :show]
     
     def index
